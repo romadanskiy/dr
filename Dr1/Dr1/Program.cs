c77d752ca407d8b89073acc2be56493bf46d9722
Kirill <kirillaromad@mail.ru>
2020-09-11 22:47:42 +0300

    добавлены комментарии

--- Dr1/Dr1/Program.cs
+++ Dr1/Dr1/Program.cs
@@ -7,11 +7,14 @@ namespace Dr1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите первое число:");
             var a = GetNumber();
+            Console.WriteLine("Введите оператор (+ - * /):");
             var @operator = Console.ReadLine();
+            Console.WriteLine("Введите второе число:");
             var b = GetNumber();
             var res = Calculate(a, @operator, b);
-            Console.WriteLine(res);
+            Console.WriteLine("Результат: {0}", res);
         }
     }
 }
