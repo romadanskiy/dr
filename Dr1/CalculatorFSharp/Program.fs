ae6c1dfc98bb0edbdca37bac476345f04bfc11b6
Kirill <kirillaromad@mail.ru>
2020-09-27 17:00:38 +0300

    calculator fsharp

---
+++ Dr1/CalculatorFSharp/Program.fs
@@ -0,0 +1,24 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+let sum x y = x + y
+let subtraction x y = x - y
+let multiplay x y = x * y
+let divide x y = x / y
+
+let calculate op x y =
+    match op with
+    | "+" -> sum x y
+    | "-" -> subtraction x y
+    | "*" -> multiplay x y
+    | "/" -> divide x y
+    | _ -> raise (System.NotImplementedException("Operation is not Implement"))
+
+[<EntryPoint>]
+let main argv =
+    let x = Console.ReadLine() |> Int32.Parse
+    let op = Console.ReadLine()
+    let y = Console.ReadLine() |> Int32.Parse
+    
+    calculate op x y |> Console.WriteLine
+    0
