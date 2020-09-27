fa1c15f7687eb16bd2b60197a7a004a96529b1ef
Kirill <kirillaromad@mail.ru>
2020-09-27 18:50:57 +0300

    Calculator FSharp With Maybe Computation Expression

--- Dr1/CalculatorFSharp/Program.fs
+++ Dr1/CalculatorFSharp/Program.fs
@@ -1,23 +1,51 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
 
-open System
-let sum x y = x + y
-let minus x y = x - y
-let multiplay x y = x * y
-let divide x y = x / y
+type MaybeBuilder() =
+    
+    member this.Bind(x, f) =
+        match x with
+        | None -> None
+        | Some a -> f a
+    
+    member this.Return(x) =
+        Some x
+        
+let maybe = new MaybeBuilder()
 
-let calculate x operation y =
-    match operation with
-    | "+" -> sum x y
-    | "-" -> minus x y
-    | "*" -> multiplay x y
-    | "/" -> divide x y
-    | _ -> raise (NotImplementedException("Operation is not Implement"))
+let sum num1 num2 = Some((double)num1 + (double)num2)
 
+let subtract num1 num2 = Some(num1 - num2)
+
+let multiply num1 num2 = Some(num1 * num2)
+
+let divide num1 num2 =
+    if num2 = (double)0
+        then None
+    else
+        Some(num1 / num2)
+        
+let calculate num1 operation num2 =
+    maybe{
+        let! x = match operation with
+        | "+" -> sum num1 num2
+        | "-" -> subtract num1 num2
+        | "*" -> multiply num1 num2
+        | "/" -> divide num1 num2
+        | _ -> None
+        return x
+    }
+    
+let checkAndWrite (e:double option) =
+    if e = None
+        then Console.WriteLine("None")
+    else
+        Console.WriteLine(e.Value)
+        
 [<EntryPoint>]
 let main argv =
-    let x = Console.ReadLine() |> double
+    let num1 = Console.ReadLine() |> double
     let operation = Console.ReadLine()
-    let y = Console.ReadLine() |> double
-    calculate x operation y |> Console.WriteLine
+    let num2 = Console.ReadLine() |> double
+    let res = calculate num1 operation num2
+    checkAndWrite res
     0
