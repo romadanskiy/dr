b5bfa4e36c077faf00dec8a10d3d9ae831963953
Kirill <kirillaromad@mail.ru>
2020-09-28 16:20:30 +0300

    Calculator FSharp With Maybe Computation Expression FIX

--- Dr1/CalculatorFSharp/Program.fs
+++ Dr1/CalculatorFSharp/Program.fs
@@ -12,40 +12,64 @@ type MaybeBuilder() =
         
 let maybe = new MaybeBuilder()
 
-let sum num1 num2 = Some((double)num1 + (double)num2)
+let sum num1 num2 = Some(num1 + num2)
 
 let subtract num1 num2 = Some(num1 - num2)
 
 let multiply num1 num2 = Some(num1 * num2)
 
 let divide num1 num2 =
-    if num2 = (double)0
-        then None
+    if num2 = 0.0 then
+        None
     else
         Some(num1 / num2)
         
 let calculate num1 operation num2 =
-    maybe{
-        let! x = match operation with
-        | "+" -> sum num1 num2
-        | "-" -> subtract num1 num2
-        | "*" -> multiply num1 num2
-        | "/" -> divide num1 num2
-        | _ -> None
+    maybe {
+        let! x =
+            match operation with
+            | "+" -> sum num1 num2
+            | "-" -> subtract num1 num2
+            | "*" -> multiply num1 num2
+            | "/" -> divide num1 num2
+            | _ -> None
         return x
     }
     
-let checkAndWrite (e:double option) =
-    if e = None
-        then Console.WriteLine("None")
+let checkAndWrite (e:float option) =
+    if (e.IsNone)
+        then Console.WriteLine("Ошибка: Нельзя делить на 0")
     else
-        Console.WriteLine(e.Value)
+        Console.WriteLine("Результат: {0}", e.Value)
+    
+let getOperation() =
+    Console.WriteLine("Введите оператор:")
+    let str = Console.ReadLine()
+    if (str = "+" || str = "-" || str = "*" || str = "/") then
+        Some(str)
+    else
+        None
+        
+let getNumber() =
+    Console.WriteLine("Введите число:")
+    let str = Console.ReadLine()
+    let isNumber, n = Double.TryParse str
+    if isNumber then
+        Some(n)
+    else
+        None
         
 [<EntryPoint>]
 let main argv =
-    let num1 = Console.ReadLine() |> double
-    let operation = Console.ReadLine()
-    let num2 = Console.ReadLine() |> double
-    let res = calculate num1 operation num2
-    checkAndWrite res
+    let num1 = getNumber()
+    let operation = getOperation()
+    let num2 = getNumber()
+    
+    Console.WriteLine()
+    if (num1.IsNone) then Console.WriteLine("Ошибка: Проверьте первое число")
+    if (operation.IsNone) then Console.WriteLine("Ошибка: Неверный оператор")
+    if (num2.IsNone) then Console.WriteLine("Ошибка: Провертье второе число")
+    if not (num1.IsNone || num2.IsNone || operation.IsNone) then
+        let res = calculate num1.Value operation.Value num2.Value
+        checkAndWrite res
     0
---
+++ Dr1/CalculatorFSharpTests/Tests.fs
@@ -0,0 +1,18 @@
+﻿namespace CalculatorFSharp
+
+open System
+open NUnit.Framework.Internal
+
+module CalculatorFSharpTest =
+    
+    open NUnit.Framework
+    open Calculator
+    
+    [<TestFixture>]
+    type TestClass () =
+            
+        [<Test>]
+        member this.TwoPlusTwoIsFour() =
+            let expected = 4
+            let actual = calculate 2.0 "+" 2.0
+            Assert.Equals(expected, actual)
