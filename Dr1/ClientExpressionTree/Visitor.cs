252315e1f97989cc7fc1425a72046c3ea44aac61
Kirill <kirillaromad@mail.ru>
2020-10-26 16:14:18 +0300

    expression tree

---
+++ Dr1/ClientExpressionTree/Visitor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ClientExpressionTree
+{
+    public class Visitor : ExpressionVisitor
+    {
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            //ToDo
+            throw new Exception();
+        }
+    }
+}
