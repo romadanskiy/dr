1dbea2b8e3756786359be6b9989c19a8c4b2e989
Kirill <kirillaromad@mail.ru>
2020-10-26 16:04:03 +0300

    expression tree

---
+++ Dr1/ClientExpressionTree/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ClientExpressionTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите выражение:");
+            var exp = Console.ReadLine(); // "(2+3)/12*7+8*9"
+
+            if (string.IsNullOrEmpty(exp))
+            {
+                Console.WriteLine("Пустое выражение!");
+                return;
+            }
+
+            var tree = ExpressionTree.ParseInputExpression(exp);
+
+            Console.WriteLine("План выполнения:");
+            Console.WriteLine(tree.ToString());
+
+            var res = ExpressionTree.Calculate(tree);
+            Console.WriteLine($"Результат: {res}");
+
+        }
+    }
+}
