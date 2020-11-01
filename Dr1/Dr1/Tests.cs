b0e6458c6eb27b12aee8dafdc58631fa162586aa
Kirill <kirillaromad@mail.ru>
2020-11-01 23:14:37 +0300

    update c# calculator

--- Dr1/Dr1/Tests.cs
+++ Dr1/Dr1/Tests.cs
@@ -6,25 +6,31 @@ namespace Dr1
     [TestFixture]
     public class CalculatorTests
     {
-        [TestCase(1, "+", 2, 3)]
-        [TestCase(-3, "+", 0.5, -2.5)]
-        [TestCase(6, "*", 6, 36)]
-        [TestCase(3, "*", 5, 15)]
-        [TestCase(7, "-", 8, -1)]
-        [TestCase(1.5, "-", 2, -0.5)]
-        [TestCase(6, "/", 3, 2)]
-        [TestCase(5, "/", 2, 2.5)]
-        [TestCase(1, "/", 0, double.PositiveInfinity)]
-        public void CalculateTest(double a, string @operator, double b, double expectedResult)
+        private static readonly Calculator calculator = new Calculator();
+        
+        [TestCase(1, "+", 2, 3, TestName = "1 + 2 = 3")]
+        [TestCase(-3, "+", 0.5, -2.5, TestName = "-3 + 0.5 = -2.5")]
+        [TestCase(6, "*", 6, 36, TestName = "6 + 6 = 36")]
+        [TestCase(3, "*", 0.5, 1.5, TestName = "3 * 0.5 = 1.5")]
+        [TestCase(7, "-", 8, -1, TestName = "7 - 8 = -1")]
+        [TestCase(1.5, "-", 2, -0.5, TestName = "1.5 - 2 = -0.5")]
+        [TestCase(6, "/", 3, 2, TestName = "6 / 3 = 2")]
+        [TestCase(5, "/", 2, 2.5, TestName = "5 / 2 = 2.5")]
+        public void CalculateTest(double num1, string operation, double num2, double expectedResult)
         {
-            Assert.AreEqual(expectedResult, Calculator.Calculate(a, @operator, b));
+            Assert.AreEqual(expectedResult, calculator.Calculate(num1, operation, num2));
+        }
+
+        [Test]
+        public void DivisionByZero()
+        {
+            Assert.Throws<ArgumentException>(() => calculator.Calculate(1, "/", 0));
         }
 
         [Test]
         public void WrongOperator()
         {
-            Assert.Throws<NotSupportedException>(() => Calculator.Calculate(8, "#", 5));
+            Assert.Throws<NotSupportedException>(() => calculator.Calculate(8, "#", 5));
         }
-        
     }
 }
