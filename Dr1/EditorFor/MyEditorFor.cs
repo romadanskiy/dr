3e6e33d06fce573a8113f7445df0edd680e9df8a
Kirill <kirillaromad@mail.ru>
2020-11-16 16:32:25 +0300

    EditorFor

--- Dr1/EditorFor/MyEditorFor.cs
+++ Dr1/EditorFor/MyEditorFor.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using Microsoft.AspNetCore.DataProtection.XmlEncryption;
 using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
 namespace EditorFor
 {
     public static class MyEditorFor
     {
+        // Аналог Html.EditorFor
         public static IHtmlContent MyEditorForModel(this IHtmlHelper helper, object model)
         {
             var content = new HtmlContentBuilder();
@@ -20,7 +23,7 @@ namespace EditorFor
             return content;
         }
         
-        private static readonly HashSet<Type> ProcessedTypes = new HashSet<Type>();
+        // Словарь подджерживаемых input типов
         private static readonly Dictionary<Type, string> InputTypes = new Dictionary<Type, string>
         {
             {typeof(int), "number"},
@@ -29,36 +32,39 @@ namespace EditorFor
             {typeof(string), "text"}
         };
 
-        public static IEnumerable<string> GetForm(object obj)
+        // Возвращает список блоков div с input для определенного свойства или всей моедли
+        private static IEnumerable<string> GetForm(object obj)
         {
             var type = obj.GetType();
-            ProcessedTypes.Add(type);
             foreach (var str in type
                 .GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                .SelectMany(p => Process(p, obj)))
+                .SelectMany(p => Process(new PropertyNode(p, obj, new Type[0]))))
                 yield return str;
         }
         
-        private static IEnumerable<string> Process(PropertyInfo property, object obj)
+        // Генерирует html код
+        private static IEnumerable<string> Process(PropertyNode node)
         {
-            var type = property.PropertyType;
-            yield return $"<div class='editor-label'><label for='{property.Name}'>{property.Name}</label></div>";
+            var type = node.Property.PropertyType;
+            yield return $"<div class='editor-label'><label for='{node.Property.Name}'>{node.Property.Name}</label></div>";
 
             if (InputTypes.Keys.Contains(type))
             {
-                yield return GetInput(property, obj);
+                yield return GetInput(node.Property, node.Parent);
             }
             else if (type.IsEnum)
             {
-                yield return GetSelect(property, obj);
+                yield return GetSelect(node.Property, node.Parent);
             }
             else if (type.IsClass)
             {
-                CheckType(type);
-                ProcessedTypes.Add(type);
+                node.CheckType(type);
+                node.AddType(type);
                 foreach (var str in type
                     .GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                    .SelectMany(p => Process(p, property.GetValue(obj))))
+                    .SelectMany(p => Process(new PropertyNode(p, node.GetValue(), node
+                        .PreviousNodeTypes
+                        .Append(node.Property.PropertyType)))))
                     yield return str;
             }
             else
@@ -66,7 +72,8 @@ namespace EditorFor
                 throw new NotSupportedException("Модель содержит неподдерживаемые свойства");
             }
         }
-
+        
+        // Генерирует input с определенным типом
         private static string GetInput(PropertyInfo property, object obj)
         {
             return $"<div class='editor-field'>" +
@@ -78,7 +85,8 @@ namespace EditorFor
                    $"{Checked(property, obj)}>" +
                    $"</div>";
         }
-
+        
+        // Генерирует select со всеми option для Enum
         private static string GetSelect(PropertyInfo property, object obj)
         {
             return $"<div class='editor-field'>" +
@@ -93,21 +101,50 @@ namespace EditorFor
                    $"</div>";
         }
 
+        // Добавляет свойство selected для option, если данный option выбран
         private static string Selected(PropertyInfo property, object obj, string option)
         {
             return property.GetValue(obj).ToString() == option ? " selected" : "";
         }
 
+        // Добавляет свойство checked для checkbox, если значения поля true
         private static string Checked(PropertyInfo property, object obj)
         {
             return property.PropertyType == typeof(bool) && 
                    (bool) property.GetValue(obj) ? " checked" : "";
         }
+    }
+
+    // Узел дерева свойств. Используется для проверки зацикливания 
+    // Перед добавлением узла в дерево поднимаемся вверх по его ветке, если нашли совпадение => зацикливание
+    class PropertyNode
+    {
+        public HashSet<Type> PreviousNodeTypes { get; set; }
+        public PropertyInfo Property { get; set; }
+        public object Parent { get; set; }
         
-        private static void CheckType(Type type)
+
+        public PropertyNode(PropertyInfo property, object parent, IEnumerable<Type> previousNodeTypes)
         {
-            if (ProcessedTypes.Contains(type))
+            Property = property;
+            Parent = parent;
+            PreviousNodeTypes = new HashSet<Type>(previousNodeTypes);
+        }
+        
+        public void CheckType(Type type)
+        {
+            if (PreviousNodeTypes.Contains(type))
                 throw new NotSupportedException("Произошло зацикливание");
         }
-    }
+        
+        public void AddType(Type type)
+        {
+            PreviousNodeTypes.Add(type);
+        }
+
+        public object GetValue()
+        {
+            return Property.GetValue(Parent);
+        }
+    } 
 }
