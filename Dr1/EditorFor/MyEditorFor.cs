4a0da10528ed8422fec94bbf10858e73ddd21f51
Kirill <kirillaromad@mail.ru>
2020-11-16 16:48:18 +0300

    fix EditorFor

--- Dr1/EditorFor/MyEditorFor.cs
+++ Dr1/EditorFor/MyEditorFor.cs
@@ -33,38 +33,36 @@ namespace EditorFor
         };
 
         // Возвращает список блоков div с input для определенного свойства или всей моедли
-        private static IEnumerable<string> GetForm(object obj)
+        public static IEnumerable<string> GetForm(object obj)
         {
             var type = obj.GetType();
+            var root = new PropertyNode(type, obj);
             foreach (var str in type
                 .GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                .SelectMany(p => Process(new PropertyNode(p, obj, new Type[0]))))
+                .SelectMany(p => Process(new PropertyNode(p, root, p.GetValue(obj)))))
                 yield return str;
         }
         
         // Генерирует html код
         private static IEnumerable<string> Process(PropertyNode node)
         {
-            var type = node.Property.PropertyType;
             yield return $"<div class='editor-label'><label for='{node.Property.Name}'>{node.Property.Name}</label></div>";
 
-            if (InputTypes.Keys.Contains(type))
+            if (InputTypes.Keys.Contains(node.Type))
             {
-                yield return GetInput(node.Property, node.Parent);
+                yield return GetInput(node);
             }
-            else if (type.IsEnum)
+            else if (node.Type.IsEnum)
             {
-                yield return GetSelect(node.Property, node.Parent);
+                yield return GetSelect(node);
             }
-            else if (type.IsClass)
+            else if (node.Type.IsClass)
             {
-                node.CheckType(type);
-                node.AddType(type);
-                foreach (var str in type
+                node.Parent.CheckType(node.Type);
+                foreach (var str in node
+                    .Type
                     .GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                    .SelectMany(p => Process(new PropertyNode(p, node.GetValue(), node
-                        .PreviousNodeTypes
-                        .Append(node.Property.PropertyType)))))
+                    .SelectMany(p => Process(new PropertyNode(p, node, p.GetValue(node.Value)))))
                     yield return str;
             }
             else
@@ -74,44 +72,44 @@ namespace EditorFor
         }
         
         // Генерирует input с определенным типом
-        private static string GetInput(PropertyInfo property, object obj)
+        private static string GetInput(PropertyNode node)
         {
             return $"<div class='editor-field'>" +
                    $"<input " +
-                   $"type='{InputTypes[property.PropertyType]}' " +
-                   $"id='{property.Name}' " +
-                   $"name='{property.Name}' " +
-                   $"value='{property.GetValue(obj)}'" +
-                   $"{Checked(property, obj)}>" +
+                   $"type='{InputTypes[node.Type]}' " +
+                   $"id='{node.Property.Name}' " +
+                   $"name='{node.Property.Name}' " +
+                   $"value='{node.Value}'" +
+                   $"{Checked(node)}>" +
                    $"</div>";
         }
         
         // Генерирует select со всеми option для Enum
-        private static string GetSelect(PropertyInfo property, object obj)
+        private static string GetSelect(PropertyNode node)
         {
             return $"<div class='editor-field'>" +
-                   $"<select name='{property.Name}'>" +
-                   property
-                       .PropertyType
+                   $"<select name='{node.Property.Name}'>" +
+                   node
+                       .Type
                        .GetEnumNames()
                        .Select(option =>
-                           $"<option value='{option}'{Selected(property, obj, option)}>{option}</option>")
+                           $"<option value='{option}'{Selected(node, option)}>{option}</option>")
                        .Aggregate((current, next) => current + next) +
                    $"</select>" +
                    $"</div>";
         }
 
         // Добавляет свойство selected для option, если данный option выбран
-        private static string Selected(PropertyInfo property, object obj, string option)
+        private static string Selected(PropertyNode node, string option)
         {
-            return property.GetValue(obj).ToString() == option ? " selected" : "";
+            return node.Value.ToString() == option ? " selected" : "";
         }
 
         // Добавляет свойство checked для checkbox, если значения поля true
-        private static string Checked(PropertyInfo property, object obj)
+        private static string Checked(PropertyNode node)
         {
-            return property.PropertyType == typeof(bool) && 
-                   (bool) property.GetValue(obj) ? " checked" : "";
+            return node.Type == typeof(bool) && 
+                   (bool) node.Value ? " checked" : "";
         }
     }
 
@@ -119,32 +117,32 @@ namespace EditorFor
     // Перед добавлением узла в дерево поднимаемся вверх по его ветке, если нашли совпадение => зацикливание
     class PropertyNode
     {
-        public HashSet<Type> PreviousNodeTypes { get; set; }
         public PropertyInfo Property { get; set; }
-        public object Parent { get; set; }
+        public PropertyNode Parent { get; set; }
+        public object Value { get; set; }
         
+        public Type Type { get; set; }
 
-        public PropertyNode(PropertyInfo property, object parent, IEnumerable<Type> previousNodeTypes)
+        public PropertyNode(Type type, object obj)
+        {
+            Value = obj;
+            Type = obj.GetType();
+        }
+        
+        public PropertyNode(PropertyInfo property, PropertyNode parent, object obj)
         {
             Property = property;
             Parent = parent;
-            PreviousNodeTypes = new HashSet<Type>(previousNodeTypes);
+            Value = obj;
+            Type = Property.PropertyType;
         }
         
         public void CheckType(Type type)
         {
-            if (PreviousNodeTypes.Contains(type))
+            if (Type == type)
                 throw new NotSupportedException("Произошло зацикливание");
-        }
-        
-        public void AddType(Type type)
-        {
-            PreviousNodeTypes.Add(type);
-        }
 
-        public object GetValue()
-        {
-            return Property.GetValue(Parent);
+            Parent?.CheckType(type);
         }
     } 
 }
