7ef30a9cda4045d580ff9e15724dda61cbcd873f
Kirill <kirillaromad@mail.ru>
2020-11-29 20:56:48 +0300

    add own dymanic ExpressionVisitor

---
+++ Dr1/ClientExpressionTree/DynamicExpressionVisitor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Dynamic;
+
+namespace ClientExpressionTree
+{
+    public abstract class DynamicExpressionVisitor
+    {
+        protected DynamicExpressionVisitor()
+        {
+        }
+
+        protected virtual Expression Visit(Expression node)
+        {
+            return node == null ? null : VisitExpression((dynamic) node);
+        }
+        
+        // to override
+        protected virtual Expression VisitExpression(BinaryExpression node)
+        {
+            return node;
+        }
+
+        // to override
+        protected virtual Expression VisitExpression(ConstantExpression node)
+        {
+            return node;
+        }
+    }
+}
