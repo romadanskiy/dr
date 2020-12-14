6d2fabbbe794b9751beaaf768aae29c0394bc1bc
Kirill <kirillaromad@mail.ru>
2020-12-14 15:10:49 +0300

    benchmarking

---
+++ Dr1/BenchmarksTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace BenchmarksTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Benchmarks>();
+        }
+    }
+}
---
+++ Dr1/BenchmarksTest/Testing.cs
@@ -0,0 +1,97 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace BenchmarksTest
+{
+   public class MyClass
+    {
+        public override string ToString()
+        {
+            return "s";
+        }
+    }
+    
+    public class Benchmarks
+    {
+        public void Cycle(object obj)
+        {
+            var s = obj.ToString();
+            var testString = "";
+            for (var i = 0; i < 100; i++)
+            {
+                testString += s;
+            }
+        }
+
+        [Benchmark(Description = "PublicMethod")]
+        public void InvokePubicMethod()
+        {
+            Public();
+        }
+        
+        public void Public()
+        {
+            Cycle("s");
+        }
+
+        [Benchmark(Description = "VirtualMethod")]
+        public void InvokeVirtualMethod()
+        {
+            VirtualMethod();
+        }
+        
+        public virtual void VirtualMethod()
+        {
+            Cycle("s");
+        }
+
+        [Benchmark(Description = "StaticMethod")]
+        public void InvokeStaticMethod()
+        {
+            Static();
+        }
+        
+        public static void Static()
+        {
+            var s = "s";
+            var testString = "";
+            for (var i = 0; i < 100; i++)
+            {
+                testString += s;
+            }
+        }
+
+        [Benchmark(Description = "GenericMethod")]
+        public void InvokeGenericMethod()
+        {
+            Generic(new MyClass());
+        }
+        
+        public void Generic<T>(T s)
+            where T : MyClass
+        {
+            Cycle(s.ToString());
+        }
+
+        [Benchmark(Description = "DynamicMethod")]
+        public void InvokeDynamicMethod()
+        {
+            Dynamic(new MyClass());
+        }
+        
+        public void Dynamic(dynamic obj)
+        {
+            Cycle(obj);
+        }
+
+        [Benchmark(Description = "ReflectionMethod")]
+        public void InvokeReflectionMethod()
+        {
+            ReflectionMethod();
+        }
+        
+        public void ReflectionMethod()
+        {
+            GetType().GetMethod("Cycle").Invoke(new Benchmarks(), new [] {"s"});
+        }
+    }
+}
